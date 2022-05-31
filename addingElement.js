7a2d515f6ab8496266c09e5d0fa36b42e47ed7e9
go367it <satyamkumar3001@gmail.com>
2022-05-31 23:16:43 +0530

    added function for adding an element given index

--- addingElement.js
+++ addingElement.js
@@ -9,4 +9,16 @@ const addElementLast = (num) =>{
     }
 }
 
-addElementLast(6) // calling the function
+// addElementLast(6) // calling the function
+
+const addElementIndex = (index, num) =>{
+    for(let i = arr.length; i > index-1; i--){
+        arr[i] = arr[i-1]
+    }
+    arr[index] = num
+    for(let i = 0; i<arr.length; i++){
+        console.log(arr[i]," ");
+    }
+}
+
+addElementIndex(2, 10)
