5dae857ebe0f1d322f804a950d81a981f2ba3d75
go367it <satyamkumar3001@gmail.com>
2022-06-02 23:46:19 +0530

    array rotation

---
+++ arrayRotation.js
@@ -0,0 +1,12 @@
+let arr = [1,2,3,4,5,6,7]
+
+// function for rotating the array
+const arrayRotationSimple = (swapValue) =>{
+
+    let tempArr = []
+
+    for(let i = 0; i<swapValue; i++){
+        tempArr[i] = arr[i]
+    }
+
+}
