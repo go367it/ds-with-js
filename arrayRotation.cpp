fc1623c88f8fe830fb1c1aacaae0488630c3ce63
go367it <satyamkumar3001@gmail.com>
2022-06-07 15:31:30 +0530

    added test file for exams

--- arrayRotation.cpp
+++ arrayRotation.cpp
@@ -1,17 +1,44 @@
+// simple method
 #include <iostream>
 using namespace std;
 
 // function for rotating the array
-void arrayRotation(int arr[]){
+void arrayRotation(int arr[], int n, int swapNumber)
+{
+    int newArray[swapNumber];
 
+    // taking the elements out and storing them in another array
+    for (int i = 0; i < swapNumber; i++)
+    {
+        newArray[i] = arr[i];
+    }
+
+    for (int i = 0; i < swapNumber; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            int temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+        }
+    }
+
+    for (int i = 0; i < swapNumber; i++)
+    {
+        for (int j = n - swapNumber; j < n; j++)
+        {
+            arr[j] = newArray[i];
+        }
+    }
 }
 
+// main function
 int main()
 {
-    int n;
+    int n, swapNumber; // for array limit
     cout << "Enter the limit for the array: ";
     cin >> n;
-    int arr[n];
+    int arr[n]; // initiliazing the array
     cout << '\n';
 
     // taking the user input
@@ -20,7 +47,10 @@ int main()
         cin >> arr[i];
     }
 
-    arrayRotation(arr);
-    
+    cout << "Enter swapping number: ";
+    cin >> swapNumber;
+
+    arrayRotation(arr, n, swapNumber); // calling the function and passing the parameters
+
     return 0;
 }
